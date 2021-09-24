0b9a227199bb69fbde85f0ae32db1604dab9068d
justcodelap <91322132+justcodelap@users.noreply.github.com>
2021-09-24 14:24:24 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from bs4 import BeautifulSoup
 from posixpath import expanduser
 from bs4.element import SoupStrainer
@@ -7,43 +6,60 @@ from mysql.connector import Error
 from typing import Text
 import requests
 import csv
-
-url = "https://www.cineplex.de/programm/aichach/"
-urll = "https://www.cineplex.de/aichach/"
-soup = BeautifulSoup(urll, 'html.parser')
-ressponse = requests.get(urll)
-response = requests.get(url)
-htmll = BeautifulSoup(ressponse.text, 'html.parser')
-html = BeautifulSoup(response.text, 'html.parser')
-quotes_html = htmll.find_all('div', {"class": "title"})
-time = htmll.find_all('div', {"class": "date"})
-date = htmll.find_all('div', {"class": "time"})
-for link in html.find_all('a'):
-    print(link.get('href'))
-
-quotes = list()
+from time import time, sleep
 mydb = mysql.connector.connect(host='localhost',
-                               database='filme',
-                               user='root',
-                               password='')
-# for quotes_html in zip(quotes):
-for i, j, k in zip(quotes_html, time, date):
-    mycursor = mydb.cursor()
-    sql = "INSERT INTO sheesh (film, time, date) VALUES (%s, %s, %s)"
-    val = (f"{i.get_text()}", f"{j.get_text()}", f"{k.get_text()}")
-    mycursor.execute(sql, val)
-    print(val)
+                                    database='filme',
+                                    user='root',
+                                    password='')
+mycursorrr = mydb.cursor()
+while True:
+    sleep(3500)
+    print("hallo")
+    mycursorr = mydb.cursor()
+    delete = "DELETE FROM `sheesh` WHERE 1"
+    mycursorr.execute(delete)
+    exit
+    while True:
+        sleep(3600)
+        url = "https://www.cineplex.de/programm/aichach/"
+        urll = "https://www.cineplex.de/aichach/"
+        soup = BeautifulSoup(urll, 'html.parser')
+        ressponse = requests.get(urll)
+        response = requests.get(url)
+        htmll = BeautifulSoup(ressponse.text, 'html.parser')
+        html = BeautifulSoup(response.text, 'html.parser')
+        quotes_html = htmll.find_all('div', {"class": "title"})
+        time = htmll.find_all('div', {"class": "date"})
+        date = htmll.find_all('div', {"class": "time"})
+        for link in html.find_all('a'):
+            print(link.get('href'))
 
-try:
+        quotes = list()
+        # for quotes_html in zip(quotes):
+        for i, j, k in zip(quotes_html, time, date):
+            mycursor = mydb.cursor()
+            sql = "INSERT INTO sheesh (film, time, date) VALUES (%s, %s, %s)"
+            val = (f"{i.get_text()}", f"{j.get_text()}", f"{k.get_text()}")
+            mycursor.execute(sql, val)
+            print(val)
 
-    mydb.commit()
+        try:
 
-    print("1 record inserted, ID:", mycursor.lastrowid)
+            mydb.commit()
 
-except mysql.connector.Error as error:
-    print("Failed to insert record into film table {}".format(error))
+            print("1 record inserted, ID:", mycursor.lastrowid)
 
-finally:
-    if mydb.is_connected():
-        mydb.close()
-        print("MySQL connection is closed")
+        except mysql.connector.Error as error:
+            print("Failed to insert record into film table {}".format(error))
+
+        finally:
+            if mydb.is_connected():
+                mydb.close()
+                print("MySQL connection is closed")
+        while True:
+            sleep(3650)
+            for i, j, k in zip(quotes_html, time, date):
+                sqlll = "INSERT INTO sheesh (film, time, date) VALUES (%s, %s, %s)"
+                vall = (f"{i.get_text()}",
+                        f"{j.get_text()}", f"{k.get_text()}")
+                mycursorrr.execute(sqlll, vall)
